7482295b854ea3b094ef20d95f0b54ba7f2dba94
mr6231 <117918828+mr6231@users.noreply.github.com>
2022-11-10 16:43:19 +0100

    kokolkoko

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Vremenko.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,5 +25,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
