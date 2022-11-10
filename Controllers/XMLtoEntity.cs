aca0b3e479d99e693f5978cf1a06fd26bc827eea
mr6231 <117918828+mr6231@users.noreply.github.com>
2022-11-10 16:41:27 +0100

    jože1

---
+++ Controllers/XMLtoEntity.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Vremenko.Models;
+using System.Xml;
+
+namespace Vremenko.Controllers;
+
+public class XMLtoEntity : Controller
+{
+    public static void Read(string url)
+    {
+        XmlTextReader reader = new XmlTextReader (url);
+
+        while (reader.Read())
+        {
+            // Do some work here on the data.
+            Console.WriteLine(reader.Name);
+        }
+        Console.ReadLine();
+    }
+
+
+
+}
